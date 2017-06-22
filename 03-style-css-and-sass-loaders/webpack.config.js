f203b6496c288f5466fc49b46efc0d1bce1ff93f
Edwin Chen <edwinchen85@gmail.com>
2017-06-22 17:12:37 +0800

    Add extract-text-webpack-plugin into config file

--- 03-style-css-and-sass-loaders/webpack.config.js
+++ 03-style-css-and-sass-loaders/webpack.config.js
@@ -1,4 +1,5 @@
 var HtmlWebpackPlugin = require('html-webpack-plugin');
+var ExtractTextPlugin = require('extract-text-webpack-plugin');
 
 module.exports = {
   entry: './src/app.js',
@@ -8,15 +9,29 @@ module.exports = {
   },
   module: {
     rules: [
-      {test: /\.scss$/, use: ['style-loader', 'css-loader', 'sass-loader']}
+      {
+        test: /\.scss$/,
+        use: ExtractTextPlugin.extract({
+          fallbackLoader: 'style-loader',
+          loader: ['css-loader', 'sass-loader'],
+          publicPath: '/dist'
+        })
+      }
     ]
   },
-  plugins: [new HtmlWebpackPlugin({
+  plugins: [
+    new HtmlWebpackPlugin({
     title: 'Project Demo',
     minify: {
       collapseWhitespace: true,
     },
     hash: true,
     template: './src/index.html',  // Load a custom template
-  })]
+  }),
+    new ExtractTextPlugin({
+      filename: 'app.css',
+      disabled: false,
+      allChunks: true
+    })
+  ]
 }
