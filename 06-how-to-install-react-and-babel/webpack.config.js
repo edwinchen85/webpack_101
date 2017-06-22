7fb16167b410c6ba0123a4b4ed45645cfb291d91
Edwin Chen <edwinchen85@gmail.com>
2017-06-22 22:42:38 +0800

    Add 06-how-to-install-react-and-babel starter files

---
+++ 06-how-to-install-react-and-babel/webpack.config.js
@@ -0,0 +1,44 @@
+var HtmlWebpackPlugin = require('html-webpack-plugin');
+var ExtractTextPlugin = require('extract-text-webpack-plugin');
+var path = require('path');
+
+module.exports = {
+  entry: './src/app.js',
+  output: {
+    path: path.resolve(__dirname, "dist"),
+    filename: 'app.bundle.js'
+  },
+  module: {
+    rules: [
+      {
+        test: /\.scss$/,
+        use: ExtractTextPlugin.extract({
+          fallbackLoader: 'style-loader',
+          loader: ['css-loader', 'sass-loader'],
+          publicPath: '/dist'
+        })
+      }
+    ]
+  },
+  devServer: {
+    contentBase: path.join(__dirname, "dist"),
+    compress: true,
+    stats: "errors-only",   // Replace verbose message with errors-only display
+    open: true,     // open browser on first run
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+    title: 'Project Demo',
+    minify: {
+      collapseWhitespace: true,
+    },
+    hash: true,
+    template: './src/index.html',  // Load a custom template
+  }),
+    new ExtractTextPlugin({
+      filename: 'app.css',
+      disabled: false,
+      allChunks: true
+    })
+  ]
+}
