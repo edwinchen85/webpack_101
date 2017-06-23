13b7a9fafcaa2c7f70aeaecc291c233ff85e98a9
Edwin Chen <edwinchen85@gmail.com>
2017-06-23 19:47:23 +0800

    Add pug-html-loader into config file

--- 08-how-to-use-pug-templates/webpack.config.js
+++ 08-how-to-use-pug-templates/webpack.config.js
@@ -25,6 +25,10 @@ module.exports = {
         test: /\.js$/,
         exclude: /node_modules/,
         use: 'babel-loader'
+      },
+      {
+        test: /\.pug$/,
+        use: 'pug-html-loader'
       }
     ]
   },
