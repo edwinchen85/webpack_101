4c269e78c884f1c9b35ed04bd704dc746dc4053b
Edwin Chen <edwinchen85@gmail.com>
2017-06-23 20:22:03 +0800

    Add 09-hot-module-replacement starter files

---
+++ 09-hot-module-replacement/webpack.config.js
@@ -0,0 +1,53 @@
+var HtmlWebpackPlugin = require('html-webpack-plugin');
+var ExtractTextPlugin = require('extract-text-webpack-plugin');
+var path = require('path');
+
+module.exports = {
+  entry: {
+    app: './src/app.js',
+    contact: './src/contact.js'
+  },
+  output: {
+    path: path.resolve(__dirname, "dist"),
+    filename: '[name].bundle.js'  // Dynamic name
+  },
+  module: {
+    rules: [
+      {
+        test: /\.scss$/,
+        use: ExtractTextPlugin.extract({
+          fallbackLoader: 'style-loader',
+          loader: ['css-loader', 'sass-loader'],
+          publicPath: '/dist'
+        })
+      },
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: 'babel-loader'
+      },
+      {
+        test: /\.pug$/,
+        use: 'pug-html-loader'
+      }
+    ]
+  },
+  devServer: {
+    contentBase: path.join(__dirname, "dist"),
+    compress: true,
+    stats: "errors-only",   // Replace verbose message with errors-only display
+    open: true,     // open browser on first run
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+    title: 'Project Demo',
+    hash: true,
+    template: './src/index.pug'
+  }),
+  new ExtractTextPlugin({
+    filename: 'app.css',
+    disable: false,
+    allChunks: true
+  })
+  ]
+}
