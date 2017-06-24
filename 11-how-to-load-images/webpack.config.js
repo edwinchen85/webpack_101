f5c03d57189ab210f12e411dbdcdcea26189e09d
Edwin Chen <edwinchen85@gmail.com>
2017-06-24 15:59:08 +0800

    Add 11-how-to-load-images starter files

---
+++ 11-how-to-load-images/webpack.config.js
@@ -0,0 +1,61 @@
+var HtmlWebpackPlugin = require('html-webpack-plugin');
+var ExtractTextPlugin = require('extract-text-webpack-plugin');
+var webpack = require('webpack');
+var path = require('path');
+
+var isProd = process.env.NODE_ENV === 'production';   // true or false
+var cssDev = ['style-loader', 'css-loader', 'sass-loader'];
+var cssProd = ExtractTextPlugin.extract({
+          fallback: 'style-loader',
+          loader: ['css-loader', 'sass-loader'],
+          publicPath: '/dist'
+        });
+var cssConfig = isProd ? cssProd : cssDev;
+
+module.exports = {
+  entry: {
+    app: './src/app.js'
+  },
+  output: {
+    path: path.resolve(__dirname, "dist"),
+    filename: '[name].bundle.js'  // Dynamic name
+  },
+  module: {
+    rules: [
+      {
+        test: /\.scss$/,
+        use: cssConfig
+      },
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: 'babel-loader'
+      },
+      {
+        test: /\.pug$/,
+        use: 'pug-html-loader'
+      }
+    ]
+  },
+  devServer: {
+    contentBase: path.join(__dirname, "dist"),
+    compress: true,
+    stats: "errors-only",   // Replace verbose message with errors-only display
+    hot: true,      // enable hot module replacement
+    open: true,     // open browser on first run
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+    title: 'Project Demo',
+    hash: true,
+    template: './src/index.pug'
+  }),
+  new ExtractTextPlugin({
+    filename: 'app.css',
+    disable: !isProd,
+    allChunks: true
+  }),
+  new webpack.HotModuleReplacementPlugin(),
+  new webpack.NamedModulesPlugin()
+  ]
+}
