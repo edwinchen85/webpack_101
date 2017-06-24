d49f8507febbdb292c96723f7369a57e639c7018
Edwin Chen <edwinchen85@gmail.com>
2017-06-24 21:52:20 +0800

    Add 13-how-to-optimize-css-stylesheet starter files

---
+++ 13-how-to-optimize-css-stylesheet/webpack.config.js
@@ -0,0 +1,77 @@
+var HtmlWebpackPlugin = require('html-webpack-plugin');
+var ExtractTextPlugin = require('extract-text-webpack-plugin');
+var webpack = require('webpack');
+var path = require('path');
+var bootstrapEntryPoints =require('./webpack.bootstrap.config');
+
+var isProd = process.env.NODE_ENV === 'production';   // true or false
+var cssDev = ['style-loader', 'css-loader', 'sass-loader'];
+var cssProd = ExtractTextPlugin.extract({
+          fallback: 'style-loader',
+          loader: ['css-loader', 'sass-loader'],
+          publicPath: '/dist'
+        });
+var cssConfig = isProd ? cssProd : cssDev;
+
+var bootstrapConfig = isProd ? bootstrapEntryPoints.prod : bootstrapEntryPoints.dev;
+
+module.exports = {
+  entry: {
+    app: './src/app.js',
+    bootstrap: bootstrapConfig
+  },
+  output: {
+    path: path.resolve(__dirname, "dist"),
+    filename: '[name].bundle.js'  // Dynamic name
+  },
+  module: {
+    rules: [
+      {
+        test: /\.scss$/,
+        use: cssConfig
+      },
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: 'babel-loader'
+      },
+      {
+        test: /\.pug$/,
+        use: 'pug-html-loader'
+      },
+      {
+        test: /\.(jpe?g|png|gif|svg)$/i,
+        use: [
+          'file-loader?name=images/[name].[ext]',
+          // Optimization: if outputPath and publicPath is the same, we can write as above
+          // 'file-loader?name=[name].[ext]&outputPath=images/&publicPath=images/',
+          'image-webpack-loader'
+        ]
+      },
+      { test: /\.(woff2?|svg)$/, loader: 'url-loader?limit=10000&name=fonts/[name].[ext]' },
+      { test: /\.(ttf|eot)$/, loader: 'file-loader?name=fonts/[name].[ext]' },
+      { test: /bootstrap-sass[\/\\]assets[\/\\]javascripts[\/\\]/, loader: 'imports-loader?jQuery=jquery' }
+    ]
+  },
+  devServer: {
+    contentBase: path.join(__dirname, "dist"),
+    compress: true,
+    stats: "errors-only",   // Replace verbose message with errors-only display
+    hot: true,      // enable hot module replacement
+    open: true,     // open browser on first run
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+    title: 'Project Demo',
+    hash: true,
+    template: './src/index.html'
+  }),
+  new ExtractTextPlugin({
+    filename: 'css/[name].css',
+    disable: !isProd,
+    allChunks: true
+  }),
+  new webpack.HotModuleReplacementPlugin(),
+  new webpack.NamedModulesPlugin()
+  ]
+}
